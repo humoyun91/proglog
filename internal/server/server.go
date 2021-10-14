4b4f81df34d34e9cc8936717912d1767bcb9531e
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-10-14 15:00:19 -0500

    configure helm

--- internal/server/server.go
+++ internal/server/server.go
@@ -15,6 +15,8 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	grpc_peer "google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
 	"time"
@@ -68,6 +70,11 @@ func NewGRPCServer(config *Config, grpcOpts ...grpc.ServerOption) (*grpc.Server,
 	)
 
 	gsrv := grpc.NewServer(grpcOpts...)
+
+	hsrv := health.NewServer()
+	hsrv.SetServingStatus("", healthpb.HealthCheckResponse_SERVING)
+	healthpb.RegisterHealthServer(gsrv, hsrv)
+
 	srv, err := newgrpcServer(config)
 	if err != nil {
 		return nil, err
